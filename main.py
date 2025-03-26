792f6fa5d5e2b2c442599920ed0de583eef9ccea
picoxolo <luc2.gilliot@gmail.com>
2025-03-26 20:06:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.fft import fft2, ifft2
+from numpy.fft import fft2, ifft2, fftshift
 import matplotlib.pyplot as plt
 
 def whitenoise(N,M,sigma):
@@ -13,44 +13,6 @@ def printimage(img, title):
 
 #printimage(whitenoise(64,64,1),"whitenoise")
 
-def convolution(k,W):
-    N = len(W)
-    M = len(W[0])
-    U = np.array([[0.]*M for i in range(N)])
-    for i in range (N):
-        for j in range (M):
-            for l in range (N):
-                for m in range(M):
-                    U[i,j] += (k[l,m]*W[(i-l)%(N),(j-m)%(M)])
-    return(U)
-
-k = np.array([[0.]*64 for i in range(64)])
-k[0,0] = 1
-k[0,1] = 1
-
-#printimage(convolution(k,whitenoise(64,64,1)),"convolution")
-
-def autocorrelation(I,m,l):
-    N,M = I.shape
-    A=0
-    for i in range(N):
-        for j in range(M):
-            A += I[(i+m)%N,(j+l)%M]*I[i,j]
-    return(A)
-
-#print(autocorrelation(whitenoise(64,64,1),1,0))
-    
-def matriceautocorrelation(I):
-    N,M = I.shape
-    A = np.array([[0.]*M for i in range(N)])
-    for i in range(N):
-        for j in range(M):
-            A[i,j] = autocorrelation(I,i,j)
-    return(A)
-
-#printimage(matriceautocorrelation(convolution(k,whitenoise(64,64,1))),"autocorrelationwhitenoise")
-
-#%%
 k_dict ={(0,0):1, (1,0):1}
 
 def convol_dict(k,W):
@@ -75,6 +37,8 @@ def convol_arr(k,W):
                     U[i][j] += k[m,n]*W[i-m][j-n]
     return U
 
+
+
 def auto_cor(I): #U est réelle
     N,M = I.shape
     A = np.zeros((N,M))
@@ -87,6 +51,9 @@ def auto_cor(I): #U est réelle
 
 def autocor(I):
     return np.real(ifft2(np.abs(fft2(I)**2)))
+
+def convol(f,g):
+    return np.real(ifft2(fft2(f)*fft2(g)))
 
 def k0(C):
     return np.real(ifft2(np.sqrt(np.abs(fft2(C)))))
@@ -103,20 +70,13 @@ def retrouver_k (u):  #u est une liste avec n réalisations de U
     k = np.real(ifft2(np.sqrt(np.abs(fft2(gamma)))))
     return(k)
 
-#printimage(retrouver_k([whitenoise(64,64,1) for i in range (5)]),"test_whitenoise")  #test pour k=1
-w = [whitenoise(64,64,1) for i in range (10)]
-#printimage(retrouver_k([convol_arr(k_arr,w[i]) for i in range(len(w))]),"test")   # on ne retrouve pas tout à fait k
-
-def retrouver(u):  #u est une liste avec n réalisations de U
+def retrouver(u):  
     n = len(u)
-    N,M = u[0].shape
-    gamma = np.zeros((N,M))
-    for i in range(n):
-        gamma += autocor(u[i])
-    gamma = (1/(n*(N**2))) * gamma
+    N,M = u.shape
+    gamma = autocor(u)/(N*M)
     fft = np.sqrt(np.abs(fft2(gamma)))
     k0 = np.real(ifft2(fft))
-    return k0, fftshift(fft), gamma, u[0]
+    return k0, fftshift(fft), gamma, u
 
 def affiche(u):
     plt.subplot(2,2,1)
@@ -133,19 +93,6 @@ def affiche(u):
     plt.title('u')
     plt.imshow(u, cmap = 'gray')
 
-            
-
-#k1 = np.real(ifft2(np.abs(fft2(k))))
-#fft_k1 = np.abs(fft2(k))
-#printimage(fftshift(fft_k1), 'fftk1')
-#plt.imshow(k)
-
-#printimage(retrouver_k([whitenoise(64,64,1) for i in range (5)]),"test_whitenoise")  #test pour k=1
-#w = [whitenoise(64,64,1) for i in range (500)]
-#k0 = retrouver_k([convol_arr(k_arr,w[i]) for i in range(len(w))])
-#u = [convol_arr(k_arr,w[i]) for i in range(len(w))]
-#printimage(retrouver_k([convol_arr(k_arr,w[i]) for i in range(len(w))])[1],"test")   # on ne retrouve pas tout à fait k
-
 k_t = np.zeros((8,8))
 for i in range(8):
     for j in range(8):
@@ -158,10 +105,18 @@ for i in range(64):
             k_cer[i,j] = 1
 #printimage(k_cer, 'cercle')
 
-        
-wh = whitenoise(128,128,1)        
-u = convol_arr(k_cer, wh)
-printimage(u, 'convol_test')
+k = np.zeros((512,512))
+k[0,1], k[0,0], k[1,0], k[1,1] = 1,1,1,1        
 
-#affiche(u)
+k1 = np.real(ifft2(np.abs(fft2(k))))
+fft_k1 = np.abs(fft2(k))
+printimage(np.abs(fftshift(fft_k1)), 'fftk1')
+        
+wh = whitenoise(512,512,1)        
+u_con = convol(k,wh)
+
+k0, fft, gamma, u = retrouver(u_con)
+printimage(fft, 'abs_fftshift_k0')  
+
+printimage(u, 'convol_test')
 

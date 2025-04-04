ae7ba471b85e526d7b3bf74be6da4ee83c4445dc
strila4 <astrid.lacotte@ens-paris-saclay.fr>
2025-04-04 11:15:10 +0200

    Update exemples.py

--- exemples.py
+++ exemples.py
@@ -1,10 +1,10 @@
+
 import main as mp
 import numpy as np
 from numpy.fft import fft2, ifft2, fftshift
 import matplotlib.pyplot as plt
 
 wh = mp.whitenoise(2048,2048,1)
-
 '''
 k_carre_petit = np.zeros((2048,2048))
 for i in range(50):
@@ -21,6 +21,8 @@ k_exp_pca, fft_k_exp_pca, gamma_exp_pca = mp.retrouver(u_conv_pca)
 k_exp_pca_2 = mp.renormalise2(k_exp_pca) #meilleur seuil 0.00085 ?
 u_exp_pca_norma2 = mp.renormalise2(mp.convol(k_exp_pca_2,wh))
 u_pca_norma2 = mp.renormalise2(u_conv_pca)
+gamma_ther_pca = mp.autocor(k_carre_petit_conv_norme)
+print("ecart gamma exp:", mp.ecart_exp(gamma_ther_pca, gamma_exp_pca))
 print("ecart k :", mp.ecart_norme2(k_exp_pca_2,k_carre_petit_conv_norme))
 print("ecart u:", mp.ecart_norme2(u_exp_pca_norma2,u_pca_norma2))
 mp.printimage([
@@ -43,9 +45,11 @@ k_carre_grand_norme = mp.renormalise2(k_carre_grand)
 k_carre_grand_conv_norme = mp.renormalise2(mp.convol (k_carre_grand_norme, k_carre_grand_norme))
 u_conv_gca = mp.convol(k_carre_grand_conv_norme,wh)
 k_exp_gca, fft_k_exp_gca, gamma_exp_gca = mp.retrouver(u_conv_gca)
-k_exp_gca_2 = mp.renormalise2(k_exp_gca,0.000025) #meilleur seuil 2.5 * 10^-5 ?
+k_exp_gca_2 = mp.renormalise2(k_exp_gca) #meilleur seuil 2.5 * 10^-5 ?
 u_exp_gca_norma2 = mp.renormalise2(mp.convol(k_exp_gca_2,wh))
 u_gca_norma2 = mp.renormalise2(u_conv_gca)
+gamma_ther_gca = mp.autocor(k_carre_grand_conv_norme)
+print("ecart gamma exp:", mp.ecart_exp(gamma_ther_gca, gamma_exp_gca))
 print("ecart k :", mp.ecart_norme2(k_exp_gca_2,k_carre_grand_conv_norme))
 print("ecart u :", mp.ecart_norme2(u_exp_gca_norma2,u_gca_norma2))
 mp.printimage([
@@ -70,6 +74,8 @@ gamma_tpce = mp.autocor(k_cer_tres_petit_conv_norme)
 k_exp_tpce_2 = mp.renormalise2(k_exp_tpce) # meilleur seuil 7.25 * 10^-4
 u_exp_tpce_norma2 = mp.renormalise2(mp.convol(k_exp_tpce_2,wh))
 u_tpce_norma2 = mp.renormalise2(u_conv_tpce)
+gamma_ther_tpce = mp.autocor(k_cer_tres_petit_conv_norme)
+print("ecart gamma exp:", mp.ecart_exp(gamma_ther_tpce, gamma_exp_tpce))
 print("ecart k :", mp.ecart_norme2(k_exp_tpce_2,k_cer_tres_petit_conv_norme))
 print("ecart u :", mp.ecart_norme2(u_exp_tpce_norma2,u_tpce_norma2))
 print("norme2 de gamma_exp :",mp.norme2(gamma_exp_tpce),"  norme2 de gamma :",mp.norme2(gamma_tpce))
@@ -98,6 +104,8 @@ gamma_pce = mp.autocor(k_cer_petit_conv_norme)
 k_exp_pce_2 = mp.renormalise2(k_exp_pce) # meilleur seuil 8,5 * 10^-4 ?
 u_exp_pce_norma2 = mp.renormalise2(mp.convol(k_exp_pce_2,wh))
 u_pce_norma2 = mp.renormalise2(u_conv_pce)
+gamma_ther_pce = mp.autocor(k_cer_petit_conv_norme)
+print("ecart gamma exp:", mp.ecart_exp(gamma_ther_pce, gamma_exp_pce))
 print("ecart k :", mp.ecart_norme2(k_exp_pce_2,k_cer_petit_conv_norme))
 print("ecart u :", mp.ecart_norme2(u_exp_pce_norma2,u_pce_norma2))
 print("norme2 gamma_exp =",mp.norme2(gamma_exp_pce),"  norme2 gamma =",mp.norme2(gamma_pce))
@@ -126,12 +134,14 @@ gamma_gce = mp.autocor(k_cer_grand_conv_norme)
 k_exp_gce_2 = mp.renormalise2(k_exp_gce) # meilleur seuil 1,25 * 10^-4 ?
 u_exp_gce_norma2 = mp.renormalise2(mp.convol(k_exp_gce_2,wh))
 u_gce_norma2 = mp.renormalise2(u_conv_gce)
+gamma_ther_gce = mp.autocor(k_cer_grand_conv_norme)
+print("ecart gamma exp:", mp.ecart_exp(gamma_ther_gce, gamma_exp_gce))
 print("ecart k :", mp.ecart_norme2(k_exp_gce_2,k_cer_grand_conv_norme))
 print("ecart u :", mp.ecart_norme2(u_exp_gce_norma2,u_gce_norma2))
 print("norme2 gamma_exp =",mp.norme2(gamma_exp_gce),"  norme2 gamma =",mp.norme2(gamma_gce))
 print("ecart type sur gamma(x) =", (2*(mp.norme2(gamma_gce)**2)/(2048**2))**(1/2))
 print("ecart type exp gamma(x) =", mp.variance(gamma_exp_gce,gamma_gce)**(1/2))
-#mp.printimage([fftshift(gamma_gce),fftshift(gamma_exp_gce)],["gamma","gamma_exp"])
+mp.printimage([fftshift(gamma_gce),fftshift(gamma_exp_gce)],["gamma","gamma_exp"])
 mp.printimage([
     fftshift(k_cer_grand_conv_norme),fftshift(k_exp_gce_2),fftshift(np.real(fft2(k_cer_grand_conv_norme))**2),
     fftshift(np.real(fft2(k_exp_gce_2))**2),u_exp_gce_norma2,u_gce_norma2
@@ -150,6 +160,8 @@ gamma_dirac = mp.autocor(k_dirac_conv_norme)
 k_exp_dirac_2 = mp.renormalise2(k_exp_dirac)
 u_exp_dirac_norma2 = mp.renormalise2(mp.convol(k_exp_dirac_2,wh))
 u_dirac_norma2 = mp.renormalise2(u_conv_dirac)
+gamma_ther_dirac = mp.autocor(k_dirac_conv_norme)
+print("ecart gamma exp:", mp.ecart_exp(gamma_ther_dirac, gamma_exp_dirac))
 print("ecart k :", mp.ecart_norme2(k_exp_dirac_2,k_dirac_conv_norme))
 print("ecart u :", mp.ecart_norme2(u_exp_dirac_norma2,u_dirac_norma2))
 print("norme2 gamma_exp =",mp.norme2(gamma_exp_dirac),"  norme2 gamma =",mp.norme2(gamma_dirac))
@@ -161,4 +173,31 @@ mp.printimage([
     fftshift(np.real(fft2(k_exp_dirac_2))**2),u_exp_dirac_norma2,u_dirac_norma2
     ],
     ["dirac_convo_normé : k0",'k_exp','fft_dirac_convo_norme**2',"fft_kexp**2","u_exp","u"])
+'''
+'''
+k_cer_petit = np.zeros((2048,2048))
+for i in range(2048):
+    for j in range(2048):
+        if (i-1024)**2 + (j-1024)**2 <= 1000:
+            k_cer_petit[i,j] = 1
+k_cer_petit_conv = mp.convol (k_cer_petit,k_cer_petit)
+k_cer_petit_conv_norme = mp.renormalise2(k_cer_petit_conv)
+u_conv_pce = mp.convol(k_cer_petit_conv_norme,wh)
+k_exp_pce, fft_k_exp_pce, gamma_exp_pce = mp.retrouver(u_conv_pce)
+gamma_pce = mp.autocor(k_cer_petit_conv_norme)
+k_exp_pce_2 = mp.renormalise2(k_exp_pce) # meilleur seuil 8,5 * 10^-4 ?
+u_exp_pce_norma2 = mp.renormalise2(mp.convol(k_exp_pce_2,wh))
+u_pce_norma2 = mp.renormalise2(u_conv_pce)
+print("ecart k :", mp.ecart_norme2(k_exp_pce_2,k_cer_petit_conv_norme))
+print("ecart u :", mp.ecart_norme2(u_exp_pce_norma2,u_pce_norma2))
+print("norme2 gamma_exp =",mp.norme2(gamma_exp_pce),"  norme2 gamma =",mp.norme2(gamma_pce))
+print("ecart type sur gamma(x) =", (2*(mp.norme2(gamma_pce)**2)/(2048**2))**(1/2))
+print("ecart type exp gamma(x) =", mp.variance(gamma_exp_pce,gamma_pce)**(1/2))
+mp.printimage([fftshift(gamma_pce),fftshift(gamma_exp_pce)],["gamma","gamma_exp"])
+mp.printimage([
+    fftshift(k_cer_petit_conv_norme),fftshift(k_exp_pce_2),fftshift(np.real(fft2(k_cer_petit_conv_norme))**2),
+    fftshift(np.real(fft2(k_exp_pce_2))**2),u_exp_pce_norma2,u_pce_norma2
+    ],
+    ["petit_cercle_convo_normé : k0",'k_exp','fft_petit_cercle_convo_norme**2',"fft_kexp**2","u_exp","u"])
+mp.plot_norme(k_cer_petit_conv_norme, k_exp_pce_2) 
 '''

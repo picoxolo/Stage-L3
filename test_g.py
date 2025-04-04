e47b0bbbef22735aeb31c5e971beb86050caee68
picoxolo <luc2.gilliot@gmail.com>
2025-04-04 09:28:36 +0200

    Update test_g.py

--- test_g.py
+++ test_g.py
@@ -1,9 +1,25 @@
-#import main as mp
 import numpy as np
+import main as mp
 from numpy.fft import fft2, ifft2, fftshift
 import matplotlib.pyplot as plt
 from scipy import integrate
 
+#on veut P(g(X)<=a)
+def proba_exp (U,l,n): #l: demi-longueur de l'intervalle n: nombre de points impairs
+    liste = np.array([0]*n, dtype=np.float64)  #liste qui découpe 
+    M,N = np.shape(U)
+    for i in range(M):
+        for j in range(N):
+            k = int(np.ceil((2*n/l)*U[i,j])) #on trouve le premier moment ou on est plus petit est on rajoute 1 dans la case
+            if k + (n-1)//2 < n :
+                liste[k + (n-1)//2 ] += 1
+    c=0
+    for i in range(n-1): #Puis on "propage" à tous les indices supérieurs
+        c += liste[i]
+        liste[i] += c - liste[i]
+    return(liste/(N*M))
+
+print(proba_exp (mp.whitenoise(1024,1024,1),2,20))
 
 def f(x):
     return (1/np.sqrt(2*np.pi))*np.exp(-x**2/2)
@@ -27,10 +43,10 @@ def retrouver_g(tab_phi, tab_p,c,n):
     h = c/((n-1)/2)
     s = int((n-1)/2)
     N = len(tab_phi)
-    T = np.zeros(N)
+    T = np.zeros(n)
     i = 0
     j = 0
-    while i < N:
+    while i < n:
         if tab_p[i] == 0.:
             while tab_p[i] == 0.:
                 T[i] = -c
@@ -42,3 +58,12 @@ def retrouver_g(tab_phi, tab_p,c,n):
             T[i] = j*h
         i+=1
     return T
+
+#print(retrouver_g(tab_phi(5,1000),proba_exp(mp.whitenoise(2048,2048,1),4,1000),2,1000))
+
+def plot_g (T,c,n):
+    ordonnee = [(2*c/(n-1))*k for k in range ((-n+1)//2,((n+1)//2))]
+    plt.plot(T,ordonnee)
+    plt.show()
+    
+plot_g(retrouver_g(tab_phi(5,1000),proba_exp(mp.whitenoise(2048,2048,1),4,1000),2,1000),4,1000)

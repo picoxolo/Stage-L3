cd432335b8d7d85f51a7fd35201cbddc523b8fc9
picoxolo <luc2.gilliot@gmail.com>
2025-04-11 11:30:33 +0200

    Update retrouver_g.py nouvelle façon de calculer g

--- retrouver_g.py
+++ retrouver_g.py
@@ -5,94 +5,87 @@ from numpy.fft import fft2, ifft2, fftshift
 import matplotlib.pyplot as plt
 from scipy import integrate
 import pickle
+from scipy.optimize import brentq
+import bisect
 
 with open("data.pkl", "rb") as f:
     data_loaded = pickle.load(f)
-
-#on veut P(g(X)<=a)
-def proba_exp (U,l,n): #l: demi-longueur de l'intervalle n: nombre de points impairs
-    liste = np.array([0]*n, dtype=np.float64)  #liste qui découpe [-l,l] en n points
-    M,N = np.shape(U)
-    for i in range(M):
-        for j in range(N):
-            k = int(np.ceil(((n-1)/(2*l))*U[i,j])) #on trouve le premier moment ou on est plus petit est on rajoute 1 dans la case
-            if (k + (n-1)//2 <n) and (k + (n-1)//2 >= 0): 
-                liste[k + (n-1)//2 ] += 1
-    c=0
-    for i in range(n): #Puis on "propage" à tous les indices supérieurs
-        c += liste[i]
-        liste[i] += c - liste[i]
-    return(liste/(N*M)) #On obtient une liste avec n point tq l[k]=P(g(X)<=(k-(n-1)/2)*l*/(n-1))=F(g^-1(a))
+    
+def proba_exp(U,n): #n:nombre de points
+    sorted_array = np.sort(U.flatten())
+    N = len(sorted_array)
+    liste = [] #liste[0] = the upper value of the first (1/n+1) % ; liste[t]=the upper value of the first t*1/(n+1) %
+    for i in range(n):
+        liste.append(sorted_array[int(np.floor(i/(n+1)*N))])
+    return (np.array(liste))
 
 def f(x):
     return (1/np.sqrt(2*np.pi))*np.exp(-x**2/2)
 
-def tab_phi(l, n):
-    """
-    l tel que I = [-l,l], désigne la longueur du demi intervalle sur lequel on estime g
-    n désigne le nombre de points, n impair
-    """
-    h = 2*l/(n-1)
-    T = np.zeros(n)
-    s = int((n-1)/2)
-    T[s] = 0.5
-    for k in range(1,s+1):
-        T[s+k] = integrate.quad(f, -np.inf, k*h)[0]
-        T[s-k] = integrate.quad(f, -np.inf, -k*h)[0]
-    return T #on obtient un array avec les valeurs de F (phi) au points k*h
+def phi(x):
+    return(integrate.quad(f, -np.inf, x)[0])
 
-def retrouver_g(tab_phi, tab_p,l_phi):
-    N = len(tab_phi)
-    h = 2*l_phi/(N-1)
-    s = int((N-1)/2)
-    n=len(tab_p)
-    T = np.zeros(n)
-    i = 0
-    j = 0
-    while tab_p[i] == 0.:
-        T[i] = -l_phi
-        i +=1
-    while i < n:
-        while (tab_p[i] > tab_phi[j]) and (j<N-1):
-            j+=1
-        T[i] = (-s+j)*h
-        i+=1
-    return T #on obient T[k]=g^-1(k-(n-1)/2)*h)
+def tab_phi(n):
+    """
+    n désigne le nombre de points sur lequel on estime phi
+    on retourne T tq T[i] = phi^-1(i*99/n%)
+    """
+    points = [(i *  1/ (n+1))  for i in range(1, n + 1)]  # From 0% to 99%
+    T = []
+    for target in points:
+    # Define the function for which we look for roots: phi(x) - target = 0
+        inverse_x = brentq(lambda x: phi(x) - target, -10, 10)
+        T.append(inverse_x)
+    return T 
 
-def plot_g_exp (T,l,g,titre):
-    n = len(T)
-    ordonnee = np.array([(2*l/(n-1))*k for k in range ((-n+1)//2,((n+1)//2),1)])
-    lin = np.linspace(T[0],T[-1],1000)
-    plt.plot(T,ordonnee, label = "g_exp")
+def plot_g_exp (tab_p,tab_phi,g,titre):
+    n = len(tab_p)
+    ordonnee = tab_p
+    abscisse = tab_phi
+    lin = np.linspace(abscisse[0],abscisse[-1],1000)
+    plt.plot(abscisse,ordonnee, label = "g_exp")
     plt.plot(lin,g(lin), label = "g")
     plt.legend()
     plt.title(titre)
     plt.show()
 
-def inverser_g (T,U,l):
-    n=len(T)
+def find_closest_index(l, x):
+    pos = bisect.bisect_left(l, x)
+    if pos == 0:
+        return 0
+    if pos == len(l):
+        return len(l) - 1
+    before = l[pos - 1]
+    after = l[pos]
+    if abs(after - x) < abs(x - before):
+        return pos
+    else:
+        return pos - 1
+    
+def inverser_g (U,tab_p,tab_phi):
+    n=len(tab_p)
     M,N = np.shape(U)
     for i in range(M):
         for j in range(N):
-            k=int(np.floor((n-1)*U[i,j]/(2*l)))
-            U[i,j] = T[(k+(n-1)//2)]
+            r = find_closest_index(tab_p,U[i,j])
+            U[i,j] = tab_phi[r]
     return(U)
 
-
-def retrouve_tot(U, k0=0, g0=0, titre="", U0=0):
-    T=retrouver_g(data_loaded["tab_phi"],proba_exp(U,5,10001),5)
-    U_exp = inverser_g(T,U,5)
+def retrouve_tot(U, n, k0=np.array([[]]), g0=None, titre="", U0=None,a=0):
+    tab_p=proba_exp(U,n)
+    inv_phi = tab_phi(n)
+    U_exp = inverser_g(U,tab_p,inv_phi)
     k_exp = mp.retrouver(U_exp)[0]
-    n = len(T)
-    ordonnee = np.array([(2*5/(n-1))*k for k in range ((-n+1)//2,((n+1)//2),1)])
-    lin = np.linspace(T[0],T[-1],1000)
-    plt.plot(T,ordonnee, label = "g_exp")
-    if k0!=0:
+    ordonnee = tab_p
+    abscisse = inv_phi
+    lin = np.linspace(abscisse[0],abscisse[-1],1000)
+    plt.plot(abscisse,ordonnee, label = "g_exp")
+    if a!=0:
         plt.plot(lin,g0(lin), label = "g0")
         plt.title(titre)
     plt.legend()
     plt.show()  
-    if k0 !=0: 
+    if a!=0: 
         mp.printimage([U_exp,U0, fftshift(k_exp),fftshift(k0)],
               ["exp","vrai","k_exp","k0"])
     else:

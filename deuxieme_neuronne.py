80d0fb6b4ed64b35d988124391a1f267efb055da
picoxolo <luc2.gilliot@gmail.com>
2025-05-27 15:44:19 +0200

    Update deuxieme_neuronne.py

--- deuxieme_neuronne.py
+++ deuxieme_neuronne.py
@@ -43,6 +43,61 @@ def opti(Z, a):
     return k_2_opt, k_1_opt, result.fun  # tu peux aussi retourner result si tu veux plus d’infos
 
 print(opti(exg.seuil_vect(mp.whitenoise(10,10,1)),0))
+
+def noyau_gaussien(sigma):
+    k = np.zeros((31,31))
+    for i in range(31):
+        for j in range(31):
+            k[i,j] = 1/np.sqrt(2*np.pi*sigma**2) * np.exp(-((i-5)**2+(j-5)**2)/(2*sigma**2))
+    return(k)
+
+def noyau_gaussien_cached(sigma):
+    return noyau_gaussien(sigma)
+
+#print(mp.norme2(noyau_gaussien(0.40)) )
+
+def fft_lamda_droite_sigma (sigma_1,sigma_2,a):
+    return((np.abs(fft2(noyau_gaussien_cached(sigma_2)))** 2) *  fft2(rtg.F_vect(a,mp.autocor(noyau_gaussien_cached(sigma_1)))) - 1)
+
+def fft_delta_droite_sigma(sigma_1,sigma_2,a):
+    return(fft2(-1*noyau_gaussien_cached(sigma_2)) *
+           fft2(noyau_gaussien_cached(sigma_2)*noyau_gaussien_cached(sigma_2)) *
+           fft2(rtg.F_vect(a,mp.autocor(noyau_gaussien_cached(sigma_1)))))
+
+    
+def opti_sigma(Z, a):
+    M, N = Z.shape
+    def optimisation(x):
+        half = len(x) // 2
+        sigma_1 = x[0]
+        sigma_2 = x[1]
+        return (
+            mp.norme2_2(fft_lambda(Z, a) - fft_lamda_droite_sigma(sigma_1, sigma_2, a))
+            + mp.norme2_2(fft_delta(Z) - fft_delta_droite_sigma(sigma_1, sigma_2, a))
+        )
+    # Point de départ (vecteurs k_2 et k_1 mis bout à bout)
+    x0 = np.array([1,1])
+    # Optimisation
+    result = minimize(optimisation, x0, options={'disp': True}, bounds = [(0.5,10),(0.5,10)]) 
+    sigma_1_opt = result.x[0]
+    sigma_2_opt = result.x[1]
+    print(sigma_1_opt, sigma_2_opt)
+    s1 = np.linspace(0.5, 10, 30)
+    s2 = np.linspace(0.5, 10, 30)
+    Z_vals = np.zeros((30, 30))
+    for i in range(30):
+        for j in range(30):
+            Z_vals[i, j] = optimisation([s1[i], s2[j]])
+        print(i)
+    plt.contourf(s1, s2, Z_vals.T, levels=30)
+    plt.xlabel("sigma_1")
+    plt.ylabel("sigma_2")
+    plt.colorbar(label="Loss_function")
+    plt.title("Paysage de la fonction à minimiser")
+    plt.show()
+    return sigma_1_opt, sigma_2_opt, result.fun  # tu peux aussi retourner result si tu veux plus d’infos    
+
+print(opti_sigma(mp.convol(noyau_gaussien(1),exg.seuil_vect(mp.convol(noyau_gaussien(1),mp.whitenoise(31,31,1)))),0))
     
     
     

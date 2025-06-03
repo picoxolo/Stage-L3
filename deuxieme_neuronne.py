cfe0a6a65bb8f7f098615820ecc87fa8efd6e78f
picoxolo <luc2.gilliot@gmail.com>
2025-06-03 10:33:51 +0200

    Update deuxieme_neuronne.py

--- deuxieme_neuronne.py
+++ deuxieme_neuronne.py
@@ -1,26 +1,40 @@
 import main as mp
+import exemples_k as exk
 import retrouver_g as rtg
+import exemples_g as exg
 import numpy as np
 from numpy.fft import fft2, ifft2, fftshift
 from scipy.integrate import tplquad
 from scipy.stats import multivariate_normal
+from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+from functools import lru_cache
+
 
 def fft_lambda(Z,a):
-    M,N = Z.shape()
+    M,N = np.shape(Z)
     A = 1/(M*N) * mp.autocor(Z)
     p = 1 - rtg.phi(a)
-    A= A - p^2
+    A= A - p**2
     return(fft2(A))
 
 def fft_lamda_droite (k_2,k_1,a):
-    return((np.abs(fft2(k_2)) ^2) *  fft2(rtg.F(a,mp.autocor(k_1))) - 1)
+    return((np.abs(fft2(k_2))** 2) *  fft2(rtg.F_vect(a,mp.autocor(k_1))) - 1)
 
 def fft_delta(Z):
-    return(fft2(-1*Z)*fft2(Z*Z))
+    M,N = np.shape(Z)
+    return(1/(M*N) * fft2(-1*Z)*fft2(Z*Z))
 
-def fft_delta_droite(k_2,k_1):
-    return(fft2(-1*k_2) * fft2(k_2*k_2) * fft2(rtg.F(a,mp.autocor(k_1))))
+def fft_delta_droite(k_2,k_1,a):
+    return(fft2(-1*k_2) * fft2(k_2*k_2) * fft2(rtg.F_vect(a,mp.autocor(k_1))))
+
+def optiv0(Z,k_2,k_1,a):
+    M,N = np.shape(Z)
+    def optimisation(k_2,k_1):
+        return(mp.norme2_2(fft_lambda(Z,a)-fft_lamda_droite(k_2.reshape((M,N)),k_1.reshape((M,N)),a))
+               + mp.norme2_2(fft_delta(Z)-fft_delta_droite(k_2.reshape((M,N)),k_1.reshape((M,N)),a)))
+    return minimize(optimisation,np.zeros((M,N)).flatten()@np.zeros((M,N)).flatten())
+
 
 def opti(Z, a):
     M, N = Z.shape
@@ -42,12 +56,13 @@ def opti(Z, a):
     k_1_opt = result.x[M * N:].reshape((M, N))
     return k_2_opt, k_1_opt, result.fun  # tu peux aussi retourner result si tu veux plus d’infos
 
-print(opti(exg.seuil_vect(mp.whitenoise(10,10,1)),0))
+#print(opti(exg.seuil_vect(mp.whitenoise(10,10,1)),0))
+
 
 def noyau_gaussien(sigma):
-    k = np.zeros((31,31))
-    for i in range(31):
-        for j in range(31):
+    k = np.zeros((101,101))
+    for i in range(101):
+        for j in range(101):
             k[i,j] = 1/np.sqrt(2*np.pi*sigma**2) * np.exp(-((i-5)**2+(j-5)**2)/(2*sigma**2))
     return(k)
 
@@ -74,7 +89,7 @@ def opti_sigma(Z, a):
         return (
             mp.norme2_2(fft_lambda(Z, a) - fft_lamda_droite_sigma(sigma_1, sigma_2, a))
             + mp.norme2_2(fft_delta(Z) - fft_delta_droite_sigma(sigma_1, sigma_2, a))
-        )
+        ),
     # Point de départ (vecteurs k_2 et k_1 mis bout à bout)
     x0 = np.array([1,1])
     # Optimisation
@@ -82,22 +97,63 @@ def opti_sigma(Z, a):
     sigma_1_opt = result.x[0]
     sigma_2_opt = result.x[1]
     print(sigma_1_opt, sigma_2_opt)
-    s1 = np.linspace(0.5, 10, 30)
-    s2 = np.linspace(0.5, 10, 30)
-    Z_vals = np.zeros((30, 30))
-    for i in range(30):
-        for j in range(30):
+    s1 = np.linspace(0.5, 10, 5)
+    s2 = np.linspace(0.5, 10, 5)
+    Z_vals = np.zeros((5, 5))
+    for i in range(5):
+        for j in range(5):
             Z_vals[i, j] = optimisation([s1[i], s2[j]])
         print(i)
-    plt.contourf(s1, s2, Z_vals.T, levels=30)
+    plt.contourf(s1, s2, Z_vals.T, levels=5)
     plt.xlabel("sigma_1")
     plt.ylabel("sigma_2")
     plt.colorbar(label="Loss_function")
     plt.title("Paysage de la fonction à minimiser")
     plt.show()
-    return sigma_1_opt, sigma_2_opt, result.fun  # tu peux aussi retourner result si tu veux plus d’infos    
+    return sigma_1_opt, sigma_2_opt, result.fun  # retourner result pour plus d'info    
 
-print(opti_sigma(mp.convol(noyau_gaussien(1),exg.seuil_vect(mp.convol(noyau_gaussien(1),mp.whitenoise(31,31,1)))),0))
+#print(opti_sigma(mp.convol(noyau_gaussien(1),exg.seuil_vect(mp.convol(noyau_gaussien(1),mp.whitenoise(101,101,1)))),0))
+
+
+def energie(Z,k,delta):
+    return(mp.norme2_2(ifft2(fft2(Z)/(fft2(k)+delta)) - (ifft2(fft2(Z)/(fft2(k)+delta)))**2))
+
+k = np.zeros((100,100))
+k[0,0] = 1
+
+k_ini = np.ones((100,100))
+
+k_2 = np.zeros((100,100))
+k_2[0,0] = 1
+k_2[0,1] = 1
+
+k_3 = np.zeros((100,100))
+k_3[0,0] = 1
+k_3[0,1] = 1
+k_3[0,2] = 1
+k_3[1,1] = 1
+k_3[1,0] = 1
+k_3[1,2] = 1
+k_3[2,1] = 1
+k_3[2,0] = 1
+k_3[2,2] = 1
+#print(energie(exg.seuil_vect(mp.whitenoise(100,100,1)),k,0.000001))
+
+def grad_e(Z,k,delta):
+    M,N = np.shape(Z)
+    return(2/(M*N) * 
+        mp.convol((ifft2(fft2(Z)/(fft2(k)+delta)) - (ifft2(fft2(Z)/(fft2(k)+delta)))**2) * (2*ifft2(fft2(Z)/(fft2(k)+delta))- 1),
+           fft2(fft2(Z)/(fft2(k)**2+delta))))
+
+#mp.printimage([grad_e(exg.seuil_vect(mp.whitenoise(100,100,1)),k_ini,0.000001)],["grad"])
+
+def descente(Z,k,delta,eps):
+    for i in range(10000):
+        grad = grad_e(Z,k,delta)
+        k = k - grad * eps
+    return(mp.renormalise2(k))
+
+mp.printimage([descente(mp.convol(k_3,exg.seuil_vect(mp.whitenoise(100,100,1))),k_ini,0.00000000000000001,1)],["k_exp"])
     
     
     
